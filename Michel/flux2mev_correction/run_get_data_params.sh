afc2c2e3aa0476f4242659f9fc185996820376c0
littleca <littleca@l-60800090.physics.lsa.umich.edu>
2025-12-03 17:39:49 -0500

    Updated for general use

--- Michel/flux2mev_correction/run_get_data_params.sh
+++ Michel/flux2mev_correction/run_get_data_params.sh
@@ -6,66 +6,33 @@ RUNTYPE=	# 0: Sum (find the fit for all the runs at once. From file michel_pair_
 			# 1: Runs (find the fit for each run, using all of their subruns. From file michel_pair_####.root)
 			# 2: Subruns (find the fit for each subrun. From file michel_pair_r####_s####.root)
 
-VERSIONID=	# The key timestamp for this set of data, usually same as the Sum value found prior
+VERSIONID=	# The name used to label the corrections to be saved to a new directory and added to json file. Will be used in downstream analysis.
 
-ERICDATA=0	# Are we using Eric's data? This will be deleted later, probably
 
-if [ $ERICDATA -eq 0 ]; then
-	INDIR="/home/littleca/kdar/Michel/output_updateMichelPair"
-	#OUTDIR="/home/littleca/kdar/cleanerKDAR/Michel/flux2mev_correction/output_fluxCorr"
+INDIR="/home/littleca/kdar/Michel/output_updateMichelPair"
 
-	if [ $RUNTYPE -eq 0 ]; then
-		#timestamp=$(date +%s)
+if [ $RUNTYPE -eq 0 ]; then
+	#timestamp=$(date +%s)
 
-		INPUT="$INDIR/michel_pair_combined.root"
-		if [ -f $INPUT ]; then
-			python get_data_params.py "$INPUT" "$RUNTYPE" "$VERSIONID"
-		else
-			echo "ERROR: $INPUT does not exist."
-		fi
-
-	elif [ $RUNTYPE -eq 1 ]; then
-		for file in `ls $INDIR`
-		do
-			RUN=${file:12:4}
-			
-			# Check if we're only processing individual runs
-			re='^[0-9999]+$'
-			if ! [[ $RUN =~ $re ]] ; then 
-				continue
-			fi
-
-			echo "python get_data_params.py $INDIR/$file $RUNTYPE $VERSIONID"
-			python get_data_params.py "$INDIR/$file" "$RUNTYPE" "$VERSIONID"
-		done;
+	INPUT="$INDIR/michel_pair_combined.root"
+	if [ -f $INPUT ]; then
+		python get_data_params.py "$INPUT" "$RUNTYPE" "$VERSIONID"
+	else
+		echo "ERROR: $INPUT does not exist."
 	fi
 
-else	#Using Eric's data
-	INDIR="/home/littleca/kdar/Michel/old_eric_updateMichelPair"
-	OUTDIR="/home/littleca/kdar/Michel/flux2mev_correction/output_fluxCorr"
-	if [ $RUNTYPE -eq 0 ]; then
-		#timestamp=$(date +%s)
-
-		INPUT="$INDIR/michel_analy_combined.root"
-		if [ -f $INPUT ]; then
-			python get_data_params.py "$INPUT" "$RUNTYPE" "$VERSIONID"
-		else
-			echo "ERROR: $INPUT does not exist."
+elif [ $RUNTYPE -eq 1 ]; then
+	for file in `ls $INDIR`
+	do
+		RUN=${file:12:4}
+		
+		# Check if we're only processing individual runs
+		re='^[0-9999]+$'
+		if ! [[ $RUN =~ $re ]] ; then 
+			continue
 		fi
 
-	elif [ $RUNTYPE -eq 1 ]; then
-
-		for file in `ls $INDIR`
-		do
-			RUN=${file:14:4}
-			
-			re='^[0-9999]+$'
-			if ! [[ $RUN =~ $re ]]; then
-				continue
-			fi
-
-			echo "python get_data_params.py $INDIR/$file $RUNTYPE $VERSIONID"
-			python get_data_params.py "$INDIR/$file" "$RUNTYPE" "$VERSIONID"
-		done;
-	fi
+		echo "python get_data_params.py $INDIR/$file $RUNTYPE $VERSIONID"
+		python get_data_params.py "$INDIR/$file" "$RUNTYPE" "$VERSIONID"
+	done;
 fi
